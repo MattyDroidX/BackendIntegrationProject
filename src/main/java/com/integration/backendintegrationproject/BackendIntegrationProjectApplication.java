c19b2e23ea0998c5b1c9600277da7cc2284e3fc6
Matias <fernando.matias.duarte@gmail.com>
2023-02-20 13:49:00 -0300

    creating project

---
+++ src/main/java/com/integration/backendintegrationproject/BackendIntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.integration.backendintegrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIntegrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendIntegrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/integration/backendintegrationproject/BackendIntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.backendintegrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIntegrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
