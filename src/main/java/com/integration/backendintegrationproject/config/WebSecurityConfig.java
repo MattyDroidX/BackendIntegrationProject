62407c36b3fdac7cda43f8ec6149ab6e18ff9957
MattyDroidX <fernando.matias.duarte@gmail.com>
2023-04-08 17:48:50 -0300

    new security resource

--- src/main/java/com/integration/backendintegrationproject/config/WebSecurityConfig.java
+++ src/main/java/com/integration/backendintegrationproject/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
@@ -27,14 +30,18 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/api/").hasRole("USER")
-                .requestMatchers("/swagger-ui/index.html").permitAll()
+                .requestMatchers("/api/dentist", "/api/patient", "/api/appointment").hasRole("ADMIN")
+                .requestMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
                 .httpBasic();
-        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.csrf()
+                .ignoringRequestMatchers(toH2Console())
+                .ignoringRequestMatchers("/swagger-ui/**")
+                .disable();
         return http.build();
     }
 
@@ -63,10 +70,10 @@ public class WebSecurityConfig {
         return source;
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer(){
-//        return (WebMVCConfigurerAdapter) addCorsMappings(registry) -> {
-//            registry
-//        }
-//    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().requestMatchers( "/swagger-ui/**", "/v3/api-docs/**" );
+        };
+    }
 }
